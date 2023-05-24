34533e8e42a2dd64345498a5602ad3890a157eb9
diegosilvaas <diegomecanica2014@gmail.com>
2023-05-23 21:22:05 -0400

    atualizando web.php

--- LaravelFramework/routes/web.php
+++ LaravelFramework/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\JogosController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/home', function () {
-    // return view('welcome');
-// });
+
 
 // Route::view('/jogos','jogos');
 
@@ -26,6 +25,12 @@ use Illuminate\Support\Facades\Route;
 //     return view('jogos',['idJogo'=>$id, 'nomeJogo'=>$name]);
 // })->where(['id' => '[0-9]+', 'name' => '[a-z]+']);
 
-Route::get('/jogos', function () {
-    return view('jogos');
+Route::get('/jogos', [JogosController::class, 'index']);
+
+Route::get('/home', function () {
+    return view('welcome');
+})->name('home-index');
+
+Route::fallback(function(){
+    return "Erro ao localizar a rota!";
 });
