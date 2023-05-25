d0bb45745a8d8e02cbd1b5d6180fafa26a220fee
diegosilvaas <diegomecanica2014@gmail.com>
2023-05-24 23:36:05 -0400

    funcao para salvar os dados

--- LaravelFramework/app/Http/Controllers/JogosController.php
+++ LaravelFramework/app/Http/Controllers/JogosController.php
@@ -7,17 +7,22 @@ use Illuminate\Http\Request;
 
 class JogosController extends Controller
 {
-    public function index ()
-{
-    // dd('Ola mundo');
-    $jogos = Jogo::all();
-    // dd($jogos);
-    return view('jogos.index', ['jogos'=>$jogos]);
-}
+        public function index ()
+        {
+            // dd('Ola mundo');
+            $jogos = Jogo::all();
+            // dd($jogos);
+            return view('jogos.index', ['jogos'=>$jogos]);
+        }
 
-public function create()
-{
-    return view('jogos.create');
-}
+        public function create()
+        {
+            return view('jogos.create');
+        }
+        public function store(Request $request)
+        {
+            dd($request);
+        }
+
 }
 
