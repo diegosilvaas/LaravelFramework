7189d795d5598ebcfd59ab7fffbbee2fb7361683
diegosilvaas <diegomecanica2014@gmail.com>
2023-05-24 23:33:37 -0400

    criando rota para salvar os dados

--- LaravelFramework/routes/web.php
+++ LaravelFramework/routes/web.php
@@ -17,7 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('jogos')->group(function(){
     Route::get('/', [JogosController::class, 'index'])->name('jogos-index');
-    Route::get('/', [JogosController::class, 'index'])->name('jogos-create');
+    Route::get('/create', [JogosController::class, 'create'])->name('jogos-create');
+    Route::post('/', [JogosController::class, 'store'])->name('jogos-store');
+
 
 });
 
