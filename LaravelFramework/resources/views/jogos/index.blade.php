3face3d0c4a2ed3d590f8b044208fa2eecbaf3f5
diegosilvaas <diegomecanica2014@gmail.com>
2023-05-24 23:01:23 -0400

    adicionando a tabela

--- LaravelFramework/resources/views/jogos/index.blade.php
+++ LaravelFramework/resources/views/jogos/index.blade.php
@@ -3,5 +3,24 @@
 @section('title', 'Listagem' )
 
 @section('content')
-<h1> TESTE</h1>
+<h1> Listagem de jogos</h1>
+    <table class="table">
+    <thead>
+        <tr>
+        <th scope="col">#</th>
+        <th scope="col">Nome</th>
+        <th scope="col">Categoria</th>
+        <th scope="col">Ano de riação</th>
+        <th scope="col">Valor</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+        </tr>
+    </tbody>
+    </table>
 @endsection
