4626bb36959ee4053149e5f5b323c6ed43521014
Diego Ferreira <diegomecanica2014@gmail.com>
2023-05-30 00:36:21 -0400

    atualizando rotas

--- LaravelFramework/routes/web.php
+++ LaravelFramework/routes/web.php
@@ -19,9 +19,9 @@ Route::prefix('jogos')->group(function(){
     Route::get('/', [JogosController::class, 'index'])->name('jogos-index');
     Route::get('/create', [JogosController::class, 'create'])->name('jogos-create');
     Route::post('/', [JogosController::class, 'store'])->name('jogos-store');
-    Route::get('/{id}/edit', [JogosController::class, 'edit'])-> where('id', '[0,9]+')->name('jogos-edit');
-    Route::put('/{id}', [JogosController::class, 'update'])-> where('id', '[0,9]+')->name('jogos-update');
-    Route::delete('/{id}', [JogosController::class, 'destroy'])-> where('id', '[0,9]+')->name('jogos-destroy');
+    Route::get('/{id}/edit', [JogosController::class, 'edit'])->name('jogos-edit');
+    Route::put('/{id}', [JogosController::class, 'update'])->name('jogos-update');
+    Route::delete('/{id}', [JogosController::class, 'destroy'])->name('jogos-destroy');
 
 
 });
